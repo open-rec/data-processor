abdbd21cb7fd66783cb03395ba97d5acb745a22c
xsank <xsank@foxmail.com>
2022-11-01 17:36:10 +0800

    init flink job

--- flink/src/main/java/com/openrec/dp/flink/EtlJob.java
+++ flink/src/main/java/com/openrec/dp/flink/EtlJob.java
@@ -1,8 +1,60 @@
 package com.openrec.dp.flink;
 
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.redis.RedisSink;
+import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisCommandDescription;
+import org.apache.flink.streaming.connectors.redis.common.mapper.RedisMapper;
+
+@Slf4j
 public class EtlJob {
 
     public static void main(String[] args) {
 
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(Runtime.getRuntime().availableProcessors());
+
+        KafkaSource<String> kafkaSource = KafkaSource.<String>builder()
+                .setBootstrapServers("")
+                .setTopics("")
+                .setGroupId("")
+                .setStartingOffsets(OffsetsInitializer.latest())
+                .setValueOnlyDeserializer(new SimpleStringSchema())
+                .build();
+
+        DataStream<String> dataStream = env.fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), "kafka");
+
+        FlinkJedisPoolConfig jedisPoolConfig = new FlinkJedisPoolConfig.Builder()
+                .build();
+        RedisSink<String> redisSink = new RedisSink<>(jedisPoolConfig, new RedisMapper<String>() {
+            @Override
+            public RedisCommandDescription getCommandDescription() {
+                return null;
+            }
+
+            @Override
+            public String getKeyFromData(String s) {
+                return null;
+            }
+
+            @Override
+            public String getValueFromData(String s) {
+                return null;
+            }
+        });
+        dataStream.addSink(redisSink);
+
+        try {
+            env.execute();
+        } catch (Exception e) {
+            log.error("flink job execute failed:{}", ExceptionUtils.getStackTrace(e));
+        }
     }
 }
